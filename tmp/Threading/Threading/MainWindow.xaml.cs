c0f107583e251a032856e1e4353cb5b7fe4cd931
horverno@gmail.com <horverno@gmail.com@fff6b56d-a95f-2e0f-d433-d9121343cfc7>
2014-11-04 11:29:40 +0000

    Threading example program with DataBinding.

---
+++ tmp/Threading/Threading/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.ComponentModel;
+
+namespace Threading
+{
+    public class NewThread : INotifyPropertyChanged
+    {
+        Thread _t;
+        public int i { get; private set; }
+        public NewThread()
+        {
+            _t = new Thread(new ThreadStart(doWork));
+            _t.Start();
+        }
+        void doWork()
+        {
+            for (i = 1; i <= 1000; i++)
+            {
+                Thread.Sleep(1000);
+                this.NotifyPropertyCanged("i");
+                System.Diagnostics.Debug.WriteLine(i);
+            }
+        }
+        public void stop()
+        {
+            _t.Abort();
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyCanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public NewThread s = new NewThread();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = s;
+        }
+
+ 
+        private void Window_Closed_1(object sender, EventArgs e)
+        {
+            s.stop();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("aa");
+        }
+    }
+}
