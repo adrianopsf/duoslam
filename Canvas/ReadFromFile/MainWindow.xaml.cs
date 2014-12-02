fdae1c8e3ec17548bde53217081d2bbccc2930cd
kollar.dominik0@gmail.com <kollar.dominik0@gmail.com@fff6b56d-a95f-2e0f-d433-d9121343cfc7>
2014-12-02 12:48:36 +0000

    Write line on canvas

---
+++ Canvas/ReadFromFile/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReadFromFile
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public void addPoints(int x1, int y1, int x2, int y2)
+        {
+            Line myLine = new Line();
+            myLine.Stroke = System.Windows.Media.Brushes.Black;
+            myLine.SnapsToDevicePixels = true;
+            myLine.SetValue(RenderOptions.EdgeModeProperty, EdgeMode.Aliased);
+
+            myLine.X1 = x1;
+            myLine.X2 = y1;  
+            myLine.Y1 = x2;
+            myLine.Y2 = y2;
+            myLine.StrokeThickness = 1;
+            canvas.Children.Add(myLine);
+        }
+
+        public MainWindow()
+        { 
+            InitializeComponent();
+
+            addPoints(100,140,200,200);
+            addPoints(10, 180, 200, 10);
+            addPoints(150, 10, 20, 40);
+        }
+    }
+}
